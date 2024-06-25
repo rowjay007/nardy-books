649ea973b8e274331bcfa0b49b422cf9d65b1560
Rowland Adimoha <rowlives@gmail.com>
2024-06-25 10:55:36 +0100

    add error handler to paystack

--- src/utils/paystack.ts
+++ src/utils/paystack.ts
@@ -7,23 +7,24 @@ export const initializePayment = async (
   amount: number,
   email: string,
   reference: string
-) => {
-  const response = await axios.post(
-    "https://api.paystack.co/transaction/initialize",
-    {
-      amount: amount * 100,
-      email,
-      reference,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${PAYSTACK_TEST_SECRET_KEY}`,
-        "Content-Type": "application/json",
-      },
-    }
-  );
-  return response.data;
+): Promise<any> => {
+  try {
+    const response = await axios.post(
+      "https://api.paystack.co/transaction/initialize",
+      { amount: amount * 100, email, reference },
+      {
+        headers: {
+          Authorization: `Bearer ${PAYSTACK_TEST_SECRET_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to initialize payment with Paystack.");
+  }
 };
+
 
 export const verifyPayment = async (reference: string) => {
   const response = await axios.get(
