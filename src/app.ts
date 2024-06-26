c85b558a7bba96794d90fed5d8e426f809bef7ac
Rowland Adimoha <rowlives@gmail.com>
2024-06-26 09:42:39 +0100

    add content security policy

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,50 @@
-import express from "express";
-import morgan from "morgan";
-import helmet from "helmet";
-import cors from "cors";
-import limiter from "./config/rateLimiter";
 import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import limiter from "./config/rateLimiter";
+import swaggerSpec from "./config/swaggerConfig";
 import errorMiddleware from "./middlewares/errorMiddleware";
 import router from "./routes";
-import swaggerUi from "swagger-ui-express";
-import swaggerSpec from "./config/swaggerConfig";
 import AppError from "./utils/appError";
 
 const app = express();
 
-
+const corsOptions = {
+  origin: ["http://example.com", "http://www.example.com"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
 
 app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(compression());
 
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://cdnjs.cloudflare.com"],
+      styleSrc: ["'self'", "https://cdnjs.cloudflare.com"],
+      imgSrc: ["'self'", "data:"],
+      connectSrc: ["'self'"],
+      fontSrc: ["'self'", "https://cdnjs.cloudflare.com"],
+      objectSrc: ["'none'"],
+      upgradeInsecureRequests: [],
+    },
+  })
+);
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use(limiter); 
+app.use(limiter);
 
 app.use(router);
 
