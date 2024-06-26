c47aa1c26b18cdd1a0103791750d1bec0f6827cd
Rowland Adimoha <rowlives@gmail.com>
2024-06-26 09:13:19 +0100

    add rate limiter and compression

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,27 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
+import limiter from "./config/rateLimiter";
+import compression from "compression";
 import errorMiddleware from "./middlewares/errorMiddleware";
 import router from "./routes";
-import swaggerUi from "swagger-ui-express"; 
-import swaggerSpec from "./config/swaggerConfig"; 
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./config/swaggerConfig";
 import AppError from "./utils/appError";
 
 const app = express();
+
+
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(compression());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(limiter); 
 
 app.use(router);
 
