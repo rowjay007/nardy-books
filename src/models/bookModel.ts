de48fb44b2f14bf42a8d3271621c81a659c43689
Rowland Adimoha <rowlives@gmail.com>
2024-07-14 06:12:07 +0100

    implemented indexing for book model

--- src/models/bookModel.ts
+++ src/models/bookModel.ts
@@ -2,20 +2,35 @@ import mongoose, { Document, Schema, Types } from "mongoose";
 
 export interface IBook extends Document {
   title: string;
-  author: Types.ObjectId; 
-  publisher: Types.ObjectId; 
-  genre: Types.ObjectId; 
-  reviews: Types.ObjectId[]; 
+  author: Types.ObjectId;
+  publisher: Types.ObjectId;
+  genre: Types.ObjectId;
+  reviews: Types.ObjectId[];
   price: number;
 }
 
 const BookSchema: Schema<IBook> = new Schema({
-  title: { type: String, required: true },
-  author: { type: Schema.Types.ObjectId, ref: "Author", required: true },
-  publisher: { type: Schema.Types.ObjectId, ref: "Publisher", required: true },
-  genre: { type: Schema.Types.ObjectId, ref: "Genre", required: true },
+  title: { type: String, required: true, index: true }, 
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "Author",
+    required: true,
+    index: true,
+  }, 
+  publisher: {
+    type: Schema.Types.ObjectId,
+    ref: "Publisher",
+    required: true,
+    index: true,
+  }, 
+  genre: {
+    type: Schema.Types.ObjectId,
+    ref: "Genre",
+    required: true,
+    index: true,
+  }, 
   reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
-  price: { type: Number, required: true },
+  price: { type: Number, required: true, index: true }, 
 });
 
 const Book = mongoose.model<IBook>("Book", BookSchema);
