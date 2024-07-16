14cede6a59167d3e4ac6e150cdb2a52024bbef6c
Rowland Adimoha <rowlives@gmail.com>
2024-07-16 11:14:54 +0100

    chore: add catch async to user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import userService from "../services/userService";
 import AppError from "../utils/appError";
+import catchAsync from "../utils/catchAsync";
 
 /**
  * Controller function to get all users
@@ -8,12 +9,8 @@ import AppError from "../utils/appError";
  * @param res Express response object
  * @param next Express next function
  */
-export const getAllUsers = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
+export const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const filter = {};
     const sort = {};
     const limit = 10;
@@ -24,10 +21,8 @@ export const getAllUsers = async (
       status: "success",
       data: { users },
     });
-  } catch (error) {
-    next(error);
   }
-};
+);
 
 /**
  * Controller function to get a user by ID
@@ -35,26 +30,19 @@ export const getAllUsers = async (
  * @param res Express response object
  * @param next Express next function
  */
-export const getUserById = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const userId = req.params.id;
-
-  try {
+export const getUserById = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.params.id;
     const user = await userService.getUserById(userId);
     if (!user) {
-      throw new AppError("User not found", 404);
+      return next(new AppError("User not found", 404));
     }
     res.status(200).json({
       status: "success",
       data: { user },
     });
-  } catch (error) {
-    next(error);
   }
-};
+);
 
 /**
  * Controller function to update a user
@@ -62,24 +50,17 @@ export const getUserById = async (
  * @param res Express response object
  * @param next Express next function
  */
-export const updateUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const userId = req.params.id;
-  const updateData = req.body;
-
-  try {
+export const updateUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.params.id;
+    const updateData = req.body;
     const updatedUser = await userService.updateUser(userId, updateData);
     res.status(200).json({
       status: "success",
       data: { user: updatedUser },
     });
-  } catch (error) {
-    next(error);
   }
-};
+);
 
 /**
  * Controller function to delete a user
@@ -87,21 +68,14 @@ export const updateUser = async (
  * @param res Express response object
  * @param next Express next function
  */
-export const deleteUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const userId = req.params.id;
-
-  try {
+export const deleteUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.params.id;
     await userService.deleteUser(userId);
     res.status(200).json({
       status: "success",
       message: "User successfully deleted",
       data: null,
     });
-  } catch (error) {
-    next(error);
   }
-};
+);
