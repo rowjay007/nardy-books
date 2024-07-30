acd4a58073a1109ead5ccb3941a0ebed593981f4
Rowland Adimoha <rowlives@gmail.com>
2024-07-30 09:01:46 +0100

    chore(cron): fix ranking NaN error

--- src/cron/bookRanking.ts
+++ src/cron/bookRanking.ts
@@ -1,5 +1,5 @@
-import Book from "../models/bookModel";
 import logger from "../config/logger";
+import Book from "../models/bookModel";
 import { IReview } from "../models/reviewModel";
 
 export async function updateBookRankings() {
@@ -8,10 +8,21 @@ export async function updateBookRankings() {
 
     const updatePromises = books.map(async (book) => {
       const reviews = book.reviews as unknown as IReview[];
-      const averageRating =
-        reviews.reduce((sum, review) => sum + review.rating, 0) /
-        reviews.length;
-      (book as any).ranking = averageRating;
+      if (reviews.length > 0) {
+        const totalRating = reviews.reduce(
+          (sum, review) => sum + review.rating,
+          0
+        );
+        const averageRating = totalRating / reviews.length;
+        if (!isNaN(averageRating)) {
+          (book as any).ranking = averageRating;
+        } else {
+          (book as any).ranking = 0;
+          logger.warn(`Average rating is NaN for book: ${book._id}`);
+        }
+      } else {
+        (book as any).ranking = 0;
+      }
       return book.save();
     });
 
